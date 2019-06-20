36fab5a82ef28e289cd55bf7a0893f1d2aef5094
Ken Moon <ken.moon@divx.com>
2019-06-20 13:32:05 +0900

    This is Ken Test for git.

---
+++ KenTest/src/main/java/Ken_TestNG_1.java
@@ -0,0 +1,34 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Ken_TestNG_1 {
+
+	@Test(priority = 1)
+	public static void doUserReg() {
+		
+		System.out.println("Executing User Reg test");
+		Assert.fail("User not registered sucessfully");
+
+	}
+	@Test(priority = 2, dependsOnMethods = "doUserReg")
+	public static void doLogin() {
+		
+		System.out.println("Executing login test");
+
+	}
+	@Test(priority = 3, dependsOnMethods = "doUserReg",alwaysRun=true)
+	public static void thirdTest() {
+		
+		System.out.println("Executing Third test");
+
+	}
+	@Test(priority = 4, dependsOnMethods = "doUserReg")
+	public static void fourthTest() {
+		
+		System.out.println("Executing Fourth test");
+
+	}
+	
+	
+	
+}
---
+++ KenTest/src/main/java/Ken_TestNG_2.java
@@ -0,0 +1,20 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Ken_TestNG_2 {
+	@Test(priority = 1)
+	public static void K1_TEST() {
+
+		System.out.println("K1_TEST");
+		Assert.fail("K1_TEST");
+
+	}
+
+	@Test(priority = 2)
+	public static void K2_TEST() {
+
+		System.out.println("K1_TEST");
+		Assert.fail("K1_TEST");
+
+	}
+}
